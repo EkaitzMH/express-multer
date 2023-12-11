b1d553d5b49d210883605da07ca34e2e944640e3
EkaitzMH <123484558+EkaitzMH@users.noreply.github.com>
2023-12-11 15:12:21 +0000

    comita

--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
 
+const storage = multer.diskStorage({destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, 'public/uploads'))},
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix)
+      }
+})
+
+const upload = multer({ storage: storage })
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.redirect('form.html');
